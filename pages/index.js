61132c10e1835d0a151d410006f54f1e6b7b1d69
Dominika <domi_nika@hotmail.co.uk>
2022-02-25 16:00:05 +0000

    feat: fetches meetups form db

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,5 @@
-import { useEffect, useState } from "react";
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUP = [
-  {
-    id: "m1",
-    title: "first meetup",
-    image:
-      "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    address: "Wonder world",
-    description: "Our first meetup",
-  },
-  {
-    id: "m2",
-    title: "second meetup",
-    image:
-      "https://images.unsplash.com/photo-1449824913935-59a10b8d2000?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    address: "Super world",
-    description: "Our second meetup",
-  },
-];
 
 const HomePage = ({ meetups }) => {
   return <MeetupList meetups={meetups} />;
@@ -41,12 +22,32 @@ const HomePage = ({ meetups }) => {
 // use for data that does not change as frequently
 // this is faster than getServerSideProps
 export async function getStaticProps() {
-  return {
-    props: {
-      meetups: DUMMY_MEETUP,
-    },
-    revalidate: 10,
-  };
+  try {
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0.0nhje.mongodb.net/meetups?retryWrites=true&w=majority`
+    );
+
+    const db = client.db();
+
+    const collection = db.collection("meetups");
+
+    const meetups = await collection.find().toArray();
+    client.close();
+
+    return {
+      props: {
+        meetups: meetups.map((meetup) => ({
+          title: meetup.title,
+          address: meetup.address,
+          image: meetup.image,
+          id: meetup._id.toString(),
+        })),
+      },
+      revalidate: 10,
+    };
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default HomePage;
