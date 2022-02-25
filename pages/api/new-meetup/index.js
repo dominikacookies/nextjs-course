423f759b2ff6873fdf10d9bbc6c080e790250372
Dominika <domi_nika@hotmail.co.uk>
2022-02-25 13:59:28 +0000

    feat: saves new meetup to db

---
+++ pages/api/new-meetup/index.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  // use req.method to check what type of req has been sent (POST/PUT/DELETE)
+  if (req.method === "POST") {
+    try {
+      const data = req.body;
+
+      const client = await MongoClient.connect(
+        `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0.0nhje.mongodb.net/meetups?retryWrites=true&w=majority`
+      );
+
+      const db = client.db();
+
+      const meetupsCollection = db.collection("meetups");
+      const result = await meetupsCollection.insertOne(data);
+
+      client.close();
+      res.status(201).json(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,18 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  const handleSubmit = (data) => {
-    console.log(data);
+  const handleSubmit = async (meetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
   };
-  return <NewMeetupForm onAddMeetUp={handleSubmit} />;
+  return <NewMeetupForm onAddMeetup={handleSubmit} />;
 };
 
 export default NewMeetupPage;
