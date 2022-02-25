6ec0eba405ed9d6d6ce6011e7daac049c8b7e0b8
Dominika <domi_nika@hotmail.co.uk>
2022-02-25 16:00:57 +0000

    refactor: prettier updates

--- pages/api/new-meetup/index.js
+++ pages/api/new-meetup/index.js
@@ -5,17 +5,18 @@ const handler = async (req, res) => {
   if (req.method === "POST") {
     try {
       const data = req.body;
-
       const client = await MongoClient.connect(
         `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0.0nhje.mongodb.net/meetups?retryWrites=true&w=majority`
       );
 
       const db = client.db();
 
-      const meetupsCollection = db.collection("meetups");
-      const result = await meetupsCollection.insertOne(data);
+      const collection = db.collection("meetups");
+
+      const result = await collection.insertOne(data);
 
       client.close();
+
       res.status(201).json(result);
     } catch (error) {
       console.error(error);
